aca19447b0b744317e6e8476b387f07101c3fa56
Jochgem van Delft <jvandelft@pixsells.nl>
2022-05-30 16:16:45 +0800

    Initial commit

--- pages/businesses/teccare.js
+++ pages/businesses/teccare.js
@@ -18,6 +18,9 @@ const TecCare = ({ }) => {
             <Widget
                 title="TecCare BD Pte Ltd"
                 sub="From fancy promotional print products to high-quality product label printing, we offer a broad range of high-quality label printing to promote your business. We make sure that your brand’s label gets the highest attention to quality and catch people’s attention." />
+            <Widget
+                title="Our Factory and Machines"
+            />
             <TecCareEightSquareImages />
         </>
     )
